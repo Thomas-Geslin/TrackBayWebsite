3cfaafa8a094b757fc6a9213984e8177172c120b
Thomas Geslin <thomas.geslin31@gmail.com>
2025-08-29 11:04:53 +0200

    build: implement Speed Insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import Header from '../components/Header';
 import AppStoreModalMount from '../components/AppStoreModalMount';
 import Footer from '@/components/Footer';
 import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 export const metadata = {
   title: 'TrackBay — Track your monthly expenses',
@@ -42,6 +43,7 @@ export default function RootLayout({
           <AppStoreModalMount />
         </ModalProvider>
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
